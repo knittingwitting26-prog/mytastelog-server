c3eae17ae43adceae7240ce3fb17a8c92ee3bce5
Lucky0626 <kmj115711@gmail.com>
2026-07-16 21:26:13 +0900

    Initial commit

---
+++ src/main/java/com/mytastelog/server/MytastelogServerApplication.java
@@ -0,0 +1,13 @@
+package com.mytastelog.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytastelogServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytastelogServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mytastelog/server/MytastelogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytastelog.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytastelogServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
